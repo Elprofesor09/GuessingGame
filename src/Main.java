6df7901481b5647d0d6830f4a4100eaed340f3ee
youssouf <you@example.com>
2025-06-19 00:38:25 +0000

    Initial commit - jeu de devinettes en Java

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1; // entre 1 et 100
+        int numberOfTries = 0;
+        int maxTries = 10;
+        Scanner scanner = new Scanner(System.in);
+        boolean hasWon = false;
+
+        System.out.println("🎲 Bienvenue dans le jeu de devinettes !");
+        System.out.println("Devinez un nombre entre 1 et 100. Vous avez 10 essais.");
+
+        while (numberOfTries < maxTries) {
+            System.out.print("Entrez votre essai #" + (numberOfTries + 1) + ": ");
+            int userGuess = scanner.nextInt();
+            numberOfTries++;
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Trop petit !");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Trop grand !");
+            } else {
+                hasWon = true;
+                break;
+            }
+        }
+
+        if (hasWon) {
+            System.out.println("🎉 Bravo ! Vous avez deviné le bon nombre en " + numberOfTries + " essai(s) !");
+        } else {
+            System.out.println("❌ Perdu ! Le bon nombre était : " + numberToGuess);
+        }
+
+        scanner.close();
+    }
+}
+6
